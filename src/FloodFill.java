2f0ce2fbd0fa4d6e3a6d263a3e2f50e08466e6f8
Lucas <lucaslanavianna.lv@gmail.com>
2026-09-12 16:51:18 -0300

    Adicionando FloodFill com pilha, criando a Main, salvando os passos dos frames de acordo com as pinturas de pixel, sistema funcional

--- src/FloodFill.java
+++ src/FloodFill.java
@@ -1,7 +1,11 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class FloodFill {
-    public void preencherComFila(BufferedImage imagem, int x, int y, int novaCor){
+    public void preencherComFila(BufferedImage imagem, int x, int y, int novaCor) throws IOException {
         Fila fila = new Fila();
 
         Coordenada inicio = new Coordenada(x,y);
@@ -12,6 +16,10 @@ public class FloodFill {
         int corFundo = imagem.getRGB(x, y);
         int largura = imagem.getWidth();
         int altura = imagem.getHeight();
+
+        int contador = 1;
+
+        new File("frames_fila").mkdirs();
 
         while(!fila.verificaVazia()){
             Coordenada p = fila.desenfileirar();
@@ -24,10 +32,60 @@ public class FloodFill {
             }
 
             imagem.setRGB(p.x, p.y, corPintar);
+            if(contador % 100 == 0){
+                ImageIO.write(imagem, "png", new File("frames_fila/frame_"+contador+".png"));
+            }
+            contador++;
             fila.enfileirar(new Coordenada (p.x, p.y+1));
             fila.enfileirar(new Coordenada (p.x, p.y-1));
             fila.enfileirar(new Coordenada (p.x+1, p.y));
             fila.enfileirar(new Coordenada (p.x-1, p.y));
         }
+
+        ImageIO.write(imagem, "png", new File("resultado_fila.png"));
+
+        System.out.println("Imagem pintada com fila com sucesso!");
+    }
+
+    public void preencherComPilha(BufferedImage imagem, int x, int y, int novaCor) throws IOException {
+        Pilha pilha = new Pilha();
+
+        Coordenada inicio = new Coordenada(x, y);
+        pilha.empilhar(inicio);
+
+        int corPintar = novaCor;
+
+        int corFundo = imagem.getRGB(x,y);
+        int largura = imagem.getWidth();
+        int altura = imagem.getHeight();
+
+        int contador = 1;
+
+        new File("frames_pilha").mkdirs();
+
+        while(!pilha.verificaVazia()){
+            Coordenada p = pilha.desempilhar();
+            if(p.x < 0 || p.x >= largura || p.y < 0 || p.y >= altura){
+                continue;
+            }
+
+            if(imagem.getRGB(p.x, p.y) != corFundo){
+                continue;
+            }
+
+            imagem.setRGB(p.x, p.y, corPintar);
+            if(contador % 100 == 0){
+                ImageIO.write(imagem, "png", new File("frames_pilha/frame_"+contador+".png"));
+            }
+            contador++;
+            pilha.empilhar(new Coordenada (p.x, p.y+1));
+            pilha.empilhar(new Coordenada (p.x, p.y-1));
+            pilha.empilhar(new Coordenada (p.x+1, p.y));
+            pilha.empilhar(new Coordenada (p.x-1, p.y));
+        }
+
+        ImageIO.write(imagem, "png", new File("resultado_pilha.png"));
+
+        System.out.println("Imagem pintada com pilha com sucesso!");
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
-public class Main {
-    public static void main(String[] args){
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        // IOException verifica erros na hora de ler a imagem, e evita que precise
+        // resolver na main, se der problema, ele alerta e para
+
+        BufferedImage imagemOriginal = ImageIO.read(new File("matriz.png"));
+        FloodFill floodFill = new FloodFill();
+
+        floodFill.preencherComFila(imagemOriginal, 10,10,0xFFAA00FF);
+        // floodFill.preencherComPilha(imagemOriginal, 150 , 150, 0xFFAA00FF);
     }
 }
